e1ddab9167ac5ed479885ba48ff6d6680259d816
Lucas Nithael <lucasnithael0101@gmail.com>
2023-03-18 21:39:45 -0300

    Form Register

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.http import Http404
+
 
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    request_form_data = request.session.get('request_form_data')
+    form = RegisterForm(request_form_data)
+    return render(request, 'authors/pages/register_view.html', {'form': form})
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404
+    
+    POST = request.POST
+    request.session['request_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
