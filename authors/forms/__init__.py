b1fdb9a1f7d86ff316e543e7619d851b16a39e29
Lucas Nithael <lucasnithael0101@gmail.com>
2023-05-12 22:28:04 -0300

    Autenticação de usuários

---
+++ authors/forms/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+from .register_forms import RegisterForm
+from .login_forms import LoginForm
+
+
+
+
+
+"""
+arquivos __init__.py servem para fazermos pasta de comportar como um módulo,
+no caso como o arquivo forms.py, dessa forma ele carrega todos os arquivos
+importado nele e o python entende como isso fosse o módulo
+"""
--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from django.http import Http404
+from django.contrib.auth import authenticate, login
 
 
 def register_view(request):
@@ -11,13 +12,13 @@ def register_view(request):
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
         'form_action': reverse('authors:create')
-            })
+    })
 
 
 def register_create(request):
     if not request.POST:
         raise Http404
-    
+
     POST = request.POST
     '''
     Guarda os dados do form na sessão do navegador
@@ -35,8 +36,32 @@ def register_create(request):
         # limpa os dados da sessão caso o formulário seja váliado
         del (request.session['request_form_data'])
 
-    return redirect('authors:register')
+    return redirect('authors:login')
 
 
 def login_view(request):
-    return render(request, 'authors/pages/login.html')
+    form = LoginForm()
+    action = reverse('authors:login_create')
+    context = {'form': form, 'form_action': action}
+    return render(request, 'authors/pages/login.html', context)
+
+
+def login_create(request):
+    if request.method != 'POST':
+        raise Http404
+
+    form = LoginForm(request.POST)
+
+    if form.is_valid():
+        username = form.cleaned_data.get('username', '')
+        password = form.cleaned_data.get('password', '')
+        authenticate_user = authenticate(username=username, password=password)
+        if authenticate_user is not None:
+            messages.success(request, 'Successs')
+            login(request, authenticate_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect('authors:login')
