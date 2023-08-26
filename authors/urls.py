74be5e89ecdb3dadab5c85fa5f1e312ddc67ad5c
nithael <lucasnithael0101@gmail.com>
2023-08-26 15:18:40 -0300

    refactor:

--- authors/urls.py
+++ authors/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
         ),
     path(
         'dashboard/recipe/<int:id>/delete/',
-        views.recipe_delete,
+        views.DashboardRecipeDelete.as_view(),
         name="dashboard_recipe_delete"
         )
 
--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -69,3 +69,17 @@ class DashboardRecipe(View):
             )
 
         return self.render_recipe(form)
+
+    
+@method_decorator(
+        login_required(login_url='authors:login', redirect_field_name='next'),
+        name='dispatch'
+)
+class DashboardRecipeDelete(DashboardRecipe):
+    def post(self, *args, **kwargs):
+        recipe = self.get_recipe(kwargs.get('id'))
+
+        recipe.delete()
+        messages.success(self.request, 'Receita deletada com sucesso')
+        
+        return redirect('authors:dashboard')
