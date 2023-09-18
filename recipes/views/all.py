cfad7167f21d009b328d5a5dc4c815b9451fc7f7
Lucas Nithael <lucasnithael0101@gmail..com>
2023-09-17 22:51:15 -0300

    recipe tab title

--- recipes/views/all.py
+++ recipes/views/all.py
@@ -44,6 +44,15 @@ class RecipeListViewHome(RecipeListViewBase):
 class RecipeListViewCategory(RecipeListViewBase):
     template_name = 'recipes/pages/category.html'
 
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        
+        ctx.update({
+            'title': f'{ctx.get("recipes")[0].category.name} - Category'
+        })
+
+        return ctx
+
     def get_queryset(self, *args, **kwargs):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(
