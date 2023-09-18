1ecbe91660f84e1dbb88e0d5b972b682e6be3a9b
Lucas Nithael <lucasnithael0101@gmail..com>
2023-09-17 23:18:25 -0300

    repairing tests

--- recipes/tests/test_recipe_view_detail.py
+++ recipes/tests/test_recipe_view_detail.py
@@ -7,16 +7,16 @@ from .test_recipes_base import RecipeTestBase
 class RecipeDetailViewsTest(RecipeTestBase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
-            reverse('recipes:recipe', kwargs={'id': 1})
+            reverse('recipes:recipe', kwargs={'pk': 1})
         )
-        self.assertIs(view.func, views.recipe)
+        self.assertIs(view.func.view_class, views.RecipeDetail)
         '''
             - já explicado nos teste da view da home
         '''
 
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1000})
+            reverse('recipes:recipe', kwargs={'pk': 1000})
         )
         self.assertEqual(response.status_code, 404)
         '''
@@ -37,7 +37,7 @@ class RecipeDetailViewsTest(RecipeTestBase):
             reverse(
                 'recipes:recipe',
                 kwargs={
-                    'id': 1
+                    'pk': 1
                 }
             )
         )
@@ -55,7 +55,7 @@ class RecipeDetailViewsTest(RecipeTestBase):
             reverse(
                 'recipes:recipe',
                 kwargs={
-                    'id': recipe.id
+                    'pk': recipe.id
                 }
             )
         )
--- recipes/urls.py
+++ recipes/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('recipes/search/', views.RecipeListViewSearch.as_view(), name='search'),
     path('', views.RecipeListViewHome.as_view(), name='home'),
     path('recipes/category/<int:category_id>/', views.RecipeListViewCategory.as_view(), name='category'),  # noqa: E501
-    path('recipes/<int:id>/', views.recipe, name='recipe'),
+    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name='recipe'),
 ]
